1ea0556b05280e206528062a0d0bfbe672854847
Cole Brown <bigswim@gmail.com>
2012-10-03 07:29:29 +0000

    updated deployment strategy

--- project.clj
+++ project.clj
@@ -5,9 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.3"]]
+                 [compojure "1.1.3"]
+                 [ring "1.2.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.7.3"]]
   :ring {:handler premiumurlshortener.core/app}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :main premiumurlshortener.core)
 
--- src/premiumurlshortener/core.clj
+++ src/premiumurlshortener/core.clj
@@ -1,12 +1,14 @@
 (ns premiumurlshortener.core
   (:use compojure.core
-        premiumurlshortener.views)
+        premiumurlshortener.views
+        ring.adapter.jetty)
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
             [compojure.response :as response]))
 
 (defroutes main-routes
-  (GET "/" [] (index-page)))
+  (GET "/" [] (index-page))
+  (route/not-found "Page not found"))
 
-(def main (handler/site main))
+(def -main (run-jetty (handler/site main-routes) {:port 3000}))
 
