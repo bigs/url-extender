d4c801704f27e88b0c83ee7265cec5a072c1c9c5
Cole Brown <bigswim@gmail.com>
2012-10-03 06:53:07 +0000

    add leiningen dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]])
